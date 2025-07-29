77e2b387acfcf13e91073571b9ed038498f65f41
Dhaval Parmar <drparmar1405@gmail.com>
2025-07-30 00:11:21 +0530

    about datatype

---
+++ datatype.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+ int main() {
+    //int=01, char=Dhavalo, flot=1.11, long=1405, double
+
+    // string s;
+    // string s1;
+    // cin >> s >>s1;
+    // cout << s <<" " <<s1;
+
+    char ch='D';
+    cout << ch;
+
+    return 0;
+}
