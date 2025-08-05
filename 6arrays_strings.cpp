3040b20753ed337068201be64eecd2914c09bf33
Dhaval Parmar <drparmar1405@gmail.com>
2025-08-06 00:29:12 +0530

    basic arrays_strings learn

---
+++ 6arrays_strings.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    // 1D array.....
+
+    // int arr[5]; 
+    // cin >> arr[0] >> arr[1] >> arr[2] >> arr[3] >> arr[4];
+    // cout << arr[3];
+
+    //2D array.....
+
+    // arr[1][3]=45;
+    // cout << arr[1][2];
+
+    //string.....
+
+    string d ="Dhaval";
+    cout << d[3];
+
+  return 0; 
+} 
