d859ed4a3514a69484a8e6492ee892c31bdaf6fc
Dhaval Parmar <drparmar1405@gmail.com>
2025-08-06 00:31:25 +0530

    basic while loop

---
+++ 8while.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i;
+    i=1;
+    while (i<=5)
+    {
+        cout << "Dhaval"<< " "<< i << endl ;
+        i++;
+    }
+    return 0;
+}
